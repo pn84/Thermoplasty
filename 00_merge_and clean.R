a83b9d4d2b0dfe0bd8c1ab3f7e82cf539251ea87
Petr Novotny <pn84@leicester.ac.uk>
2025-01-16 16:31:31 +0000

    Read in raw data, merge and clean

--- 00_merge_and clean.R
+++ 00_merge_and clean.R
@@ -1,28 +1,58 @@
 library(tidyverse)
 library(rlist)
 
-df1 = read_csv('data/exp_20250106_134911_pages.csv')
-df2 = read_csv('data/exp_20250106_151300_pages.csv')
-df2$DB_FilterType = as.character(df2$DB_FilterType)
-thermoplasty_df = full_join(df1, df2)
+# Read Coreline AVIEW output
+page1 = read_csv('data/exp_20250106_134911_pages.csv')
+page2 = read_csv('data/exp_20250106_151300_pages.csv')
+# Correct data types
+page2$DB_FilterType = as.character(page2$DB_FilterType)
+# Correct a wrong StudyDescription
+correction = page1 %>%
+  filter(
+    DB_PatientName == 'T010008',
+    DB_StudyDescription == 'CT Chest high resolution_BASELINE') %>%
+  mutate('DB_StudyDescription' = 'CT Chest high resolution_FOLLOWUP')
+page1 = page1 %>%
+  filter(
+    !(DB_PatientName == 'T010008' & DB_StudyDescription == 'CT Chest high resolution_BASELINE')
+  ) %>%
+  bind_rows(correction)
 
-# select relevant columns
+# Select relevant columns
 cols = c(
   # headers
   "DB_PatientId",
+  "DB_ImageType",
   "DB_PatientName",
   "DB_PatientSex",
   "DB_StudyDescription",
   "DB_SeriesDescription",
-  # airways
+  # airway fields
   "FWHM_Awt-Pi10(mm)_LevelWhole_WholeLung",
   "FWHM_WallArea%Mean(%)_LevelWhole_WholeLung",
-  # density
+  # density fields
   "Mean(HU)_WholeLung",
   "LAA_Volume(%)_WholeLung"
-  # TODO other columns
 )
 
+df = page1 %>%
+  select(cols) %>%
+  # Join the coreline outputs
+  full_join(select(page2, cols)) %>%
+  # Get timepoint from StudyDescription
+  mutate('timepoint' = sapply(str_split(.$DB_StudyDescription, '_'), tail, 1)) %>%
+  group_by(DB_PatientName) %>%
+  # Get the number of timepoints for each patient
+  mutate(ntimepoints = n_distinct(timepoint)) %>%
+  filter(
+    # Drop DERIVED data
+    !str_detect(DB_ImageType, 'DERIVED'),
+    # Only include inspiration,
+    !str_detect(DB_SeriesDescription, 'EXPIRATION'),
+    # Only include patients who have both baseline and followup data
+    ntimepoints == 2,
+    # Drop one extra row without measurements
+    DB_PatientId != 'T040002'
+  )
 
-# filter keep only inspiratory row
-# filter only patients with both baseline and followup
+# TODO Download and clean clinical data
